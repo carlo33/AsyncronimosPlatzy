5ce929b1851af97a93581509109d17d1fc4d7bcf
Carlos Yaco <cyaco33@gmail.com>
2020-10-27 15:05:39 -0500

    usando callbacks y XMLHttp para hacer request

---
+++ src/callback/index.js
@@ -0,0 +1,23 @@
+////CALLBACK cuando una funcion llama a otra función
+function sum (num1, num2){
+  return num1+num2;
+}
+function calc (num1, num2, callback){
+  return callback(num1,num2);
+}
+console.log(calc(2,3,sum));
+
+
+///////otro ejemplo////////////////////
+function date (callback){
+  console.log(new Date);
+  setTimeout(function(){
+    let date = new Date;
+    callback(date);
+  },3000);
+}
+
+function printDate (dateNow){
+  console.log(dateNow);
+}
+date(printDate);
